807f74556f06288bafce22a2fe21fa6012f5583b
Ibraim Calvo <ibraim_10@hotmail.com>
2022-09-14 14:15:04 -0500

    ready to render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { builtinModules } = require('module');
 const routes = require('express').Router();
 
 routes.get('/', (req, res) =>{
-    res.send('Irad Calvo')
+    res.send('Irad Calvo Rivas')
 });
 
 module.exports = routes;
